05e6d3dd295c5c92cbb38696716607f6517114ef
Maksym Sanzharov <maxsan2@gmail.com>
2020-10-13 23:35:08 +0200

    Fix Transactional annotation.

--- src/main/java/io/github/maksymsan/persistentwebapp1/service/JpaCrudService.java
+++ src/main/java/io/github/maksymsan/persistentwebapp1/service/JpaCrudService.java
@@ -24,8 +24,8 @@ import io.github.maksymsan.persistentwebapp1.repository.NamedObjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
 @Service
